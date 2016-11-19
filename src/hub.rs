cd6bd3a07f9762043f9119fdb014deb20d72f64e
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2016-11-19 15:22:45 +0100

    Test for multiple interfaces and fix code

---
+++ src/hub.rs
@@ -0,0 +1,56 @@
+use std::net::SocketAddr;
+use std::str::from_utf8;
+use mio::udp::UdpSocket;
+
+pub struct Hub {
+  pub socket: UdpSocket
+}
+
+fn receive (socket: UdpSocket) -> Option<String> {
+  let mut buffer: [u8; 2048] = [0; 2048];
+  let result = socket.recv_from(&mut buffer);
+  drop(socket);
+
+  match result {
+    Err(e) => panic!("Receive error: {}", e),
+    Ok(opt) => match opt {
+      None => None,
+      Some((amount, source)) => {
+        println!("Received {} bytes from {}", amount, source);
+        Some(format(buffer, amount, source))
+      }
+    }
+  }
+}
+
+fn format (buffer: [u8; 2048], amount: usize, source: SocketAddr) -> String {
+  let body = from_utf8(&buffer[0..amount]).unwrap_or("{}");
+  json!({
+    "src": (source.ip().to_string()),
+    "msg": (body)
+  }).to_string()
+}
+
+impl Iterator for Hub {
+  type Item = String;
+
+  fn next(&mut self) -> Option<String> {
+    let message: String;
+
+    loop {
+      match self.socket.try_clone() {
+        Err(why) => panic!("Socket error: {}", why),
+        Ok(socket) => {
+          match receive(socket) {
+            None => continue,
+            Some(msg) => {
+              message = msg;
+              break
+            }
+          }
+        }
+      }
+    }
+    Some(message)
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
+#![feature(plugin)]
+#![plugin(json_macros)]
+
+extern crate get_if_addrs;
 extern crate mio;
+extern crate rustc_serialize;
+
+mod hub;
 
 pub use mio::udp::UdpSocket;
-use std::str::{FromStr, from_utf8};
-use std::net::SocketAddr;
+use std::net::{IpAddr, SocketAddr};
+use hub::Hub;
 
-pub fn open (address: &str) -> UdpSocket {
-  let target = SocketAddr::from_str(address).unwrap();
+pub fn open (address: &IpAddr, port: u16) -> UdpSocket {
+  let target = SocketAddr::new(*address, port);
   let attempt = UdpSocket::bind(&target);
 
   match attempt {
@@ -14,11 +21,11 @@ pub fn open (address: &str) -> UdpSocket {
   }
 }
 
-pub fn send (socket: &UdpSocket, message: &str, source: &str) {
+pub fn send (socket: &UdpSocket, message: &str) {
   socket.set_broadcast(true).unwrap();
 
   let bytes = message.to_string().into_bytes();
-  let source = SocketAddr::from_str(source).unwrap();
+  let source = socket.local_addr().unwrap();
   let result = socket.send_to(&bytes, &source);
   drop(socket);
 
@@ -34,87 +41,57 @@ pub fn listen (socket: UdpSocket) -> Hub {
   }
 }
 
-pub struct Hub {
-  socket: UdpSocket
-}
-
-impl Iterator for Hub {
-  type Item = String;
-
-  fn next(&mut self) -> Option<String> {
-    let message: String;
-
-    loop {
-      match self.socket.try_clone() {
-        Err(why) => panic!("Socket error: {}", why),
-        Ok(socket) => {
-          match receive(socket) {
-            None => continue,
-            Some(msg) => {
-              message = msg;
-              break
-            }
-          }
-        }
-      }
-    }
-    Some(message)
-  }
-}
-
-fn receive (socket: UdpSocket) -> Option<String> {
-  let mut buffer: [u8; 2048] = [0; 2048];
-  let result = socket.recv_from(&mut buffer);
-  drop(socket);
-
-  match result {
-    Err(e) => panic!("Receive error: {}", e),
-    Ok(opt) => match opt {
-      None => None,
-      Some((amount, source)) => {
-        println!("Received {} bytes from {}", amount, source);
-        Some(format(buffer, amount, source))
-      }
-    }
-  }
-}
-
-fn format (buffer: [u8; 2048], amount: usize, source: SocketAddr) -> String {
-  let body = from_utf8(&buffer[0..amount]).unwrap_or("{}");
-  format!("{{\"src\":\"{}\",\"msg\":{}}}", source.ip(), body)
-}
-
 #[cfg(test)]
 mod test {
-  use std::{thread, time};
   use super::*;
+  use std::net::{IpAddr, Ipv4Addr};
+  use std::{thread, time};
+  use get_if_addrs::{get_if_addrs, IfAddr};
+  use rustc_serialize::json::Json;
 
   #[test]
-  fn short_message() {
-    let socket = open("127.0.0.1:1905");
+  fn localhost() {
+    let socket = open(&IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1905);
     let receiver = listen(socket.try_clone().unwrap());
     thread::sleep(time::Duration::from_millis(1500));
 
-    send(&socket, "\"Hello localhost!\"", "127.0.0.1:1905");
+    send(&socket, "Hello localhost!");
 
     for received in receiver {
-      assert_eq!(received, "{\"src\":\"127.0.0.1\",\"msg\":\"Hello localhost!\"}");
+      assert_eq!(received, json!({"src":"127.0.0.1","msg":"Hello localhost!"}).to_string());
       break
     }
   }
 
   #[test]
-  fn local_network() {
-    let address = "192.168.0.255:1905";
-    let socket = open(address);
-    let receiver = listen(socket.try_clone().unwrap());
-    thread::sleep(time::Duration::from_millis(1500));
+  fn network() {
+    for iface in get_if_addrs().unwrap() {
+      match iface.addr {
+        IfAddr::V4(addr) => {
+          let target = match addr.broadcast {
+            Some(broadcast) => broadcast,
+            None => addr.ip
+          };
 
-    send(&socket, "\"Hello network!\"", address);
+          let socket = open(&IpAddr::V4(target), 2304);
+          let receiver = listen(socket.try_clone().unwrap());
+          thread::sleep(time::Duration::from_millis(1500));
 
-    for received in receiver {
-      assert_eq!(received, "{\"src\":\"127.0.0.1\",\"msg\":\"Hello network!\"}");
-      break
+          send(&socket, "Hello network!");
+
+          for received in receiver {
+            let data = Json::from_str(&received).unwrap();
+            let msg = data.as_object().unwrap().get("msg").unwrap();
+            let src = data.as_object().unwrap().get("src").unwrap();
+
+            assert_eq!(msg.to_string(), json!(("Hello network!")).to_string());
+            assert_eq!(src.to_string(), json!((addr.ip.to_string())).to_string());
+
+            break
+          }
+        },
+        IfAddr::V6(_) => continue
+      }
     }
   }
 }
