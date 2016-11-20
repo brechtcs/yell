d7f0b90b8c87bc9fde7fa397e113f39a4c79a2db
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2016-11-20 21:47:21 +0100

    Expose raw listener without loop for flexibility

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,9 @@ extern crate get_if_addrs;
 extern crate mio;
 extern crate rustc_serialize;
 
-mod hub;
-
 pub use mio::udp::UdpSocket;
 use std::net::{IpAddr, SocketAddr};
-use hub::Hub;
+use std::str;
 
 pub fn open (address: &IpAddr, port: u16) -> UdpSocket {
   let target = SocketAddr::new(*address, port);
@@ -35,31 +33,51 @@ pub fn send (socket: &UdpSocket, message: &str) {
   }
 }
 
-pub fn listen (socket: UdpSocket) -> Hub {
-  Hub {
-    socket: socket
+pub fn listen (socket: UdpSocket) -> Option<String> {
+  let mut buffer: [u8; 2048] = [0; 2048];
+  let result = socket.recv_from(&mut buffer);
+  drop(socket);
+
+  match result {
+    Err(e) => panic!("Receive error: {}", e),
+    Ok(opt) => match opt {
+      None => None,
+      Some((amount, source)) => {
+        println!("Received {} bytes from {}", amount, source);
+        Some(format(buffer, amount, source))
+      }
+    }
   }
+}
+
+fn format (buffer: [u8; 2048], amount: usize, source: SocketAddr) -> String {
+  let body = str::from_utf8(&buffer[0..amount]).unwrap_or("{}");
+  json!({
+    "src": (source.ip().to_string()),
+    "msg": (body)
+  }).to_string()
 }
 
 #[cfg(test)]
 mod test {
   use super::*;
   use std::net::{IpAddr, Ipv4Addr};
-  use std::{thread, time};
   use get_if_addrs::{get_if_addrs, IfAddr};
   use rustc_serialize::json::Json;
 
   #[test]
   fn localhost() {
     let socket = open(&IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1905);
-    let receiver = listen(socket.try_clone().unwrap());
-    thread::sleep(time::Duration::from_millis(1500));
-
     send(&socket, "Hello localhost!");
 
-    for received in receiver {
-      assert_eq!(received, json!({"src":"127.0.0.1","msg":"Hello localhost!"}).to_string());
-      break
+    loop {
+      match listen(socket.try_clone().unwrap()) {
+        None => continue,
+        Some(received) => {
+          assert_eq!(received, json!({"src":"127.0.0.1","msg":"Hello localhost!"}).to_string());
+          break
+        }
+      }
     }
   }
 
@@ -74,20 +92,21 @@ mod test {
           };
 
           let socket = open(&IpAddr::V4(target), 2304);
-          let receiver = listen(socket.try_clone().unwrap());
-          thread::sleep(time::Duration::from_millis(1500));
-
           send(&socket, "Hello network!");
 
-          for received in receiver {
-            let data = Json::from_str(&received).unwrap();
-            let msg = data.as_object().unwrap().get("msg").unwrap();
-            let src = data.as_object().unwrap().get("src").unwrap();
+          loop {
+            match listen(socket.try_clone().unwrap()) {
+              None => continue,
+              Some(received) => {
+                let data = Json::from_str(&received).unwrap();
+                let msg = data.as_object().unwrap().get("msg").unwrap();
+                let src = data.as_object().unwrap().get("src").unwrap();
 
-            assert_eq!(msg.to_string(), json!(("Hello network!")).to_string());
-            assert_eq!(src.to_string(), json!((addr.ip.to_string())).to_string());
-
-            break
+                assert_eq!(msg.to_string(), json!(("Hello network!")).to_string());
+                assert_eq!(src.to_string(), json!((addr.ip.to_string())).to_string());
+                break
+              }
+            }
           }
         },
         IfAddr::V6(_) => continue
